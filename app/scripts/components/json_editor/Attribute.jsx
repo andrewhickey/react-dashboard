43955ce9be086d4d397ecbb5db5cb1d0e44208d2
Andrew Hickey <andrewhickey@live.co.uk>
2015-03-12 00:08:11 +0000

    jsonviewer bubbling

--- app/scripts/components/json_editor/Attribute.jsx
+++ app/scripts/components/json_editor/Attribute.jsx
@@ -1,41 +1,101 @@
-import React from 'react';
+import React from 'react/addons';
 import _ from "lodash";
 
 var Attribute = React.createClass({
   
+  getInitialState() {
+    return {
+      is_editing: false,
+      attribute: this.props.attribute
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      attribute: nextProps.attribute
+    });
+  },
+
   render() {
-    const attribute = this.props.attribute;
-
-
-    return (
-      <div>
-        {this.renderAttribute(attribute)}
-      </div>
-    );
+    return this.renderAttribute(this.state.attribute);
   }, 
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    this.toggleEditing();
+  },
+
+  handleChange(e) {
+    e.preventDefault();
+    this.props.onBubble(e.target.value);
+  },
+
+  handleToggle(e) {
+    this.props.onBubble(!this.state.attribute);
+  },
+
+  toggleEditing() {
+    this.setState({is_editing: !this.state.is_editing});
+  },
+
+  handleBubblingChange(index, attribute) {
+    var changeObj = {};
+    changeObj[index] = attribute;
+    var newState = React.addons.update(this.state.attribute, {
+      $merge: changeObj
+    });
+    
+    if(this.props.onBubble) {
+      this.props.onBubble(newState);
+    } else {
+      this.setState({attribute: newState});
+      if ( this.props.onChange ) this.props.onChange(newState);
+      else console.warn('Pass an onChange function as a prop to the top level component');
+    }
+
+  },
 
   renderAttribute: function(object) {
     var type = Object.prototype.toString.call(object);
+
     switch( type ) {
       case "[object Boolean]":
+        if (this.state.is_editing) {
+          return <form className={"attribute-form"} onSubmit={this.handleSubmit}><input type="checkbox" checked={object} onChange={this.handleToggle} /></form>
+        } else {
+          return <span onDoubleClick={this.toggleEditing}>{object.toString()}</span>;
+        }
       case "[object Number]":
+        if (this.state.is_editing) {
+          return <form className={"attribute-form"} onSubmit={this.handleSubmit}><input type="number" value={object} onChange={this.handleChange} /></form>
+        } else {
+          return <span onDoubleClick={this.toggleEditing}>{object.toString()}</span>;
+        }
       case "[object String]":
-        return <div>{type}{object}</div>;
+        if (this.state.is_editing) {
+          return <form className={"attribute-form"} onSubmit={this.handleSubmit}><input type="text" value={object} onChange={this.handleChange} /></form>
+        } else {
+          return <span onDoubleClick={this.toggleEditing}>{object.toString()}</span>;
+        }
         break;
+      
       case "[object Object]":
-        return  <div>
-                  {type}
-                  {this.renderChildren(object)}
-                </div>;
+        return  <span>
+                  {"{"}<ul>
+                    {this.renderChildren(object)}
+                  </ul>{"}"}
+                </span>;
         break;
+
       case "[object Array]":
-        return  <div>
-                  {type}
-                  {this.renderChildren(object)}
-                </div>;
+        return  <span>
+                  [<ul>
+                    {this.renderChildren(object)}
+                  </ul>]
+                </span>;
         break;
       default:
-        console.log('no match', type);
+        return <span>null</span>;
         break;
     }
     return 
@@ -43,8 +103,7 @@ var Attribute = React.createClass({
 
   renderChildren: function(object) {
     return  _.map(object, function(sub_attribute, index){
-      console.log(sub_attribute);
-      return <Attribute attribute={sub_attribute} key={index} />;
+      return <li  key={index}>{index}: <Attribute attribute={sub_attribute} onBubble={this.handleBubblingChange.bind(this, index)}/></li>;
     },this);
   }
 });
--- app/scripts/components/json_editor/JsonEditor.jsx
+++ app/scripts/components/json_editor/JsonEditor.jsx
@@ -3,14 +3,17 @@ import _ from "lodash";
 import Attribute from "./Attribute.jsx";
 
 const JsonEditor = React.createClass({
+  logChange: function(attribute) {
+    console.log(attribute);
+  },
   render() {
     const cursor = this.props.cursor;
     const attribute = _.cloneDeep(cursor.get()) 
-    
+
     return (
       <div>
         <pre>{JSON.stringify(cursor.get())}</pre>
-        <div><Attribute attribute={attribute} /></div>
+        <div><Attribute attribute={attribute} onSave={this.logChange}/></div>
       </div>
     );
   }
