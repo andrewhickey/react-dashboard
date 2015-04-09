048d6f7e59ef481573c8319b82bd74b6c5bd9ad7
Andrew Hickey <andrewhickey@live.co.uk>
2015-04-09 06:42:21 +0100

    changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@ import routes from './routes';
 import stateTree from './stateTree';
 
 window.addEventListener("beforeunload", function(e){
-   localStorage.state = JSON.stringify(stateTree);
+  localStorage.state = JSON.stringify(stateTree);
 }, false);
 
 Router.run(routes, Handler => React.render(<Handler />, document.body));
